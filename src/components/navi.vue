9e07525e1581414f6a94b42b0f9f0b5f9e6b9d9b
Starink <161250119@qq.com>
2018-07-30 12:27:48 +0800

    first component

---
+++ src/components/navi.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="col-xs-12 col-md-12 whole" id="navi">
+    <img src="/static/pic/logo1_white.png" class="img-responsive logo" alt="Cinque Terre">
+
+    <nav class="navbar navbar-inverse wholeNav">
+      <div class="container-fluid" style="width: 100%">
+        <div class="collapse navbar-collapse" id="myNavbar" style="display: inline-block;font-size: 16px;width: 100%">
+          <ul class="nav navbar-nav" id="nav" style="width: 100%">
+            <li><a href="./homepage.html" style="color: dodgerblue">首页</a></li>
+            <li><a href="./invest.html">投资</a></li>
+            <li><a href="./loan.html">借款</a></li>
+            <li><a href="./guide.html">教程</a></li>
+            <li id="last" style="float: right;"><a id="signup" href="./signup.html">注册</a></li>
+            <li id="secondLast" style="float: right;"><a id="login" href="./login.html">登录</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div id="manageAccount">
+      <button style="background-color: lightskyblue">用户模式</button>
+      <button>管理模式</button>
+      <button>审核模式</button>
+      <button>退出</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navi',
+}
+// function init() {
+//   alert("boynextdoor");
+//   localStorage.ifLogin = true;
+//   localStorage.ifUnread = true;
+//
+//   if (localStorage.ifLogin){
+//     document.getElementById('last').removeChild(document.getElementById('signup'));
+//     document.getElementById('secondLast').removeChild(document.getElementById('login'));
+//     var personalCenter = document.createElement('a');
+//     personalCenter.innerText = '个人中心';
+//     personalCenter.href = './personalCenter.html';
+//     document.getElementById('secondLast').appendChild(personalCenter);
+//
+//     var message = document.createElement('img');
+//     message.className = 'message';
+//     message.src = '/static/pic/message_white.png';
+//     document.getElementById('last').appendChild(message);
+//     if (localStorage.ifUnread){
+//       message.src = '/static/pic/message_yellow.png';
+//     }
+//     $('#last').hover(
+//       function(){
+//         $('#last').css('border-bottom','3px solid transparent');
+//         $('#last').css('border-top','3px solid transparent');
+//       },
+//       function(){
+//         $('#last').css('border-bottom','3px solid transparent');
+//         $('#last').css('border-top','3px solid transparent');
+//       }
+//     );
+//     $('.message').hover(
+//       function(){
+//         message.src = '/static/pic/message_blue.png';
+//       },
+//       function(){
+//         if(localStorage.ifUnread){
+//           message.src = '/static/pic/message_yellow.png';
+//         }else{
+//           message.src = '/static/pic/message_white.png';
+//         }
+//       }
+//     );
+//
+//     var photo = document.createElement('img');
+//     photo.className = 'photo';
+//     photo.src = '/static/pic/photo.jpg';//localStorage.photoSrc;
+//     var newLi = document.createElement('li');
+//     newLi.style.cssFloat = 'right';
+//     $('#nav').append(newLi);
+//     newLi.appendChild(photo);
+//   }
+// }
+</script>
+
+
+
+<style scoped>
+  .whole{
+    padding: 0;
+    position: absolute;
+    background-color: black;
+    z-index: 1;
+  }
+  .logo{
+    max-width: 9%;
+    display: inline-block;
+    margin-left: 8%;
+    margin-top: -48px;
+  }
+  .wholeNav{
+    border-color: transparent;
+    display: inline-block;
+    min-width: 80%;
+    min-height: 30px;
+    margin-top: 5px;
+    margin-bottom: 0;
+    background-color: black;
+  }
+
+  #nav li{
+    min-width: 90px;
+    text-align: center;
+    border-top:3px solid transparent;
+    border-bottom:3px solid transparent;
+  }
+
+  #nav li:hover{
+    border-top:3px solid dodgerblue;
+    border-bottom:3px solid dodgerblue;
+  }
+
+  #nav li:hover a{
+    color:dodgerblue;
+  }
+
+</style>
+
+<style>
+  .message{
+    height:25px;
+    width: auto;
+    margin-top: 10px;
+  }
+
+  .message:hover{
+    height:25px;
+    width: auto;
+    margin-top: 10px;
+  }
+
+  .photo{
+    margin-top: 4px;
+    width: 40px;
+    height: 40px;
+    border-radius: 20px;
+    cursor: pointer;
+  }
+
+  #nav li a{
+    color: white;
+  }
+
+  #manageAccount{
+    background-color: ghostwhite;
+    border-color: #efefef;
+    position: absolute;
+    top:51px;
+    right: 4%;
+    width: 10%;
+    display: none;
+    z-index: 2;
+  }
+
+  #manageAccount button{
+    background-color: ghostwhite;
+    border: 0.5px #efefef solid;
+    width: 100%;
+  }
+
+  #manageAccount button:hover{
+    background-color: lightblue;
+    border: 0.5px #efefef solid;
+    width: 100%;
+  }
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import navi from '@/components/navi'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'navi',
+      component: navi
     }
   ]
 })
