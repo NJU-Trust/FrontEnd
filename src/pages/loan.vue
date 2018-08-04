7c8158d740e3e3065b49be8eaff34e1b08b5940c
Erica-WX <2544194336@qq.com>
2018-08-04 09:54:57 +0800

    添加组件

--- src/pages/loan.vue
+++ src/pages/loan.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
       <navi></navi>
+      <div class="back">
+        <div class="sheet">
+          <div class="chooseButton">
+            <input type="radio" name="choose" value="">消费类小额短期
+            <input type="radio" name="choose" value="">学习培训类大额长期
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
@@ -15,4 +23,18 @@
 
 <style scoped>
 
+    /*.body{
+      background-color: #5f9ea0;
+    }*/
+    .back{
+      background-color: #85d1d1;
+      width: 100%;
+      height: 800px;
+      border: 1px solid black;
+    }
+
+    .sheet{
+      margin-top: 100px;
+      margin-left:200px;
+    }
 </style>
