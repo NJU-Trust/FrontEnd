83817ebb279d0e0f3c4af768024cee34b7a99780
Erica-WX <2544194336@qq.com>
2018-08-04 09:09:33 +0800

    test

---
+++ src/pages/loan.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <navi></navi>
+    </div>
+</template>
+
+<script>
+    import navi from '@/components/navi.vue';
+    export default {
+        name: "loan"
+    }
+</script>
+
+<style scoped>
+
+</style>
