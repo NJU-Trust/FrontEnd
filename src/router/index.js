82be9fbe43b833944118e57306679a5bf7f9c9f6
161250127 <361487214@qq.com>
2018-08-19 16:51:07 +0800

    usermange

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import login from '@/pages/login'
 import loan from '@/pages/loan'
 import signup from '@/pages/signup'
 import userspace from '@/pages/userspace'
+import usermanage from '@/pages/usermanage'
 
 Vue.use(Router)
 
