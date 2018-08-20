96ac407f0cb9c00626dd6f150d756efba431c3f5
161250127 <361487214@qq.com>
2018-08-20 14:36:36 +0800

    列表查询

--- src/pages/usermanage.vue
+++ src/pages/usermanage.vue
@@ -1,63 +1,36 @@
-<template>
-  <div class="UserDetail" >
+<template id="mylist">
+  <div class="UserList">
     <img src="/static/pic/background1.png" class="img-responsive" alt="Cinque Terre" style="top: 0;z-index: -1;">
-    <div class="userdetail" style="padding:50px 0px 0px 0px;position: absolute;width: 100%;">
 
-      <div class="base-info" style="padding:10px 0px 0px 100px;">
-
-        <label>用户名称：</label>
-        <label>XXXXXXXXXXXXXX</label>
-        <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-        <label>用户等级：</label>
-        <label>XXXXXXXXXXXXXX</label>
-        <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-        <label >手机号：</label>
-        <label>XXXXXXXXXXXXXX</label>
-        <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-        <label>邮箱：</label>
-        <label>XXXXXXXXXXXXXX</label>
-        <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-        <button type="button" class="returnButton">返回列表</button>
+    <div class="pager-wrapper" v-if="pageNum < 6">
+      <div v-for="(item, index) in pages" class="item" :class='{active : active === (index + 1)}' @click="changePage(item)">
+        {{item.text}}
       </div>
-      <br/>
-      <br/>
-      <div class="append-info" style="padding:20px 0px 0px 100px;">
-        <label>标的信息</label>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <label>投资信息</label>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
+    </div>
+    <div v-else>
+      <div class="pager-wrapper" v-if="active < 4">
+        <div v-for="(item, index) in pages" class="item" :class='{active : active === (index + 1)}' @click="changePage(item)">
+          {{item.text}}
+        </div>
+        <div class="point">...</div>
+        <div class="item" @click="end">{{pageNum}}</div>
       </div>
-
-
+      <div class="pager-wrapper" v-else-if="active < (pageNum - 2)">
+        <div class="item" @click="start">1</div>
+        <div class="point">...</div>
+        <div v-for="(item, index) in pages" class="item" :class='{active : active === item}' @click="changePage(item)">
+          {{item.text}}
+        </div>
+        <div class="point">...</div>
+        <div class="item" @click="end">{{pageNum}}</div>
+      </div>
+      <div class="pager-wrapper" v-else>
+        <div class="item" @click="start">1</div>
+        <div class="point">...</div>
+        <div v-for="(item, index) in pages" class="item" :class='{active : active === item}' @click="changePage(item)">
+          {{item.text}}
+        </div>
+      </div>
     </div>
 
 
@@ -68,9 +41,85 @@
 
 </template>
 
+<script>
+  export default {
+    name: "usermanage",
+    props: {
+      pageNum: {
+        type: Number,
+        default: 10
+      },
+      activeIndex: {
+        type: Number,
+        default: 1
+      }
+    },
+    data () {
+      return {
+        pages: [],
+        active: 1
+      }
+    },
+    watch: {
+      active (val) {
+        this.init()
+      }
+    },
+    created () {
+      this.active = this.activeIndex
+      this.init()
+    },
+    methods: {
+      init () {
+        if (this.pageNum < 6) {
+          let pages = []
+          for (let i = 1; i < (this.pageNum + 1); i++) {
+            pages.push(i)
+          }
+          this.pages = pages
+        } else {
+          if (this.active < 4) {
+            let pages = [1, 2, 3]
+            this.pages = pages
+            console.log(this.pages)
+          } else if (this.active < (this.pageNum - 2)) {
+            let pages = [this.active - 1, this.active, this.active + 1]
+            this.pages = pages
+          } else {
+            let pages = [this.pageNum - 2, this.pageNum - 1, this.pageNum]
+            this.pages = pages
+          }
+        }
+      },
+      changePage (item) {
+        this.active = item
+        this.$emit('changePage', item)
+      },
+      start () {
+        this.active = 1
+        this.$emit('changePage', this.active)
+      },
+      end () {
+        this.active = this.pageNum
+        this.$emit('changePage', this.active)
+      },
+      prev () {
+        if (this.active === 1) return
+        this.active--
+        this.$emit('changePage', this.active)
+      },
+      next () {
+        if (this.active === this.pageNum) return
+        this.active++
+        this.$emit('changePage', this.active)
+      }
+    }
+  }
+</script>
+
 
 <style scoped>
-  .userdetail{
+  .UserList{
     height: 550px;
     min-width: 160px;
     min-height: 60px;
@@ -79,29 +128,42 @@
     background-color:rgba(0,50,200,0.3);
     color: white;
     border: none;
-    font-size: 13px;
+    font-size: 30px;
     font-family: "Microsoft YaHei UI";
-    top: 80px;
+    top: 0px;
     position: absolute;
     letter-spacing: 2px;
   }
 
-  .returnButton{
-    border-radius: 30px;
-    min-width: 80px;
-    min-height: 40px;
-    padding: 0px 0px;
-    line-height: 30px;
-    background-color: lightskyblue;
-    color: white;
-    border: none;
-    font-size: 18px;
-    font-family: "Microsoft YaHei UI";
-    top: 550px;
-    position: fixed;
-    width: 14%;
-    left: 43%;
-    letter-spacing: 2px;
-  }
 
+  .pager-wrapper{
+    display: -webkit-flex;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .item{
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    user-select: none;
+    font-size: 16px;
+    padding: 4px 20px;
+    border: 1px solid #d7d7d7;
+    margin-left: 16px;
+    cursor: pointer;
+    color: #a5a5a5;
+  }
+  .active{
+    background: #a5a5a5;
+    color: #fff;
+    border: 1px solid #a5a5a5;
+  }
+  .point{
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    user-select: none;
+    margin-left: 16px;
+    letter-spacing: 3px;
+    color: #a5a5a5;
+  }
 </style>
