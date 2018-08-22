504603efd8c9d780cc2fa3028f8f1482c253bb2e
Hua Meng <38884748+Dreamanne@users.noreply.github.com>
2018-08-22 23:14:11 +0800

    Add router for invest

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import NormalCheck from '@/pages/NormalCheck'
 import FinancialStanding from '@/pages/FinancialStanding'
 import usermanage from '@/pages/usermanage'
 import userdetail from '@/pages/userdetail'
+import invest from '@pages/userdetail'
 
 Vue.use(Router)
 
@@ -58,6 +59,11 @@ export default new Router({
       path: '/userdetail',
       name: 'userdetail',
       component: userdetail
+    }
+    {
+      path:'/invest',
+      name:'invest',
+      component:invest
     }
 
 
