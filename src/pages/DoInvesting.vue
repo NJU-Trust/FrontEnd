25d9414b9039e0edff5e1b5e82a897098dd77726
James <1025149919@qq.com>
2018-08-22 21:25:32 +0800

    update
    完善了进行投资界面

--- src/pages/DoInvesting.vue
+++ src/pages/DoInvesting.vue
@@ -3,59 +3,77 @@
       <navi></navi>
       <div class="back">
 
-        //返回按钮
+        <!-- 返回按钮 -->
         <div class="left">
           <div class="left_label" @click="change1">
-          <button id="backPage" v-on:click="backPage()">返回</button>
+          <el-button  icon="el-icon-back" v-on:click="backPage()"></el-button>
           </div>
         </div>
 
-        <div id="sheet" class="sheet" style="display: none">
+
+        <div id="sheet" class="sheet" style="display: inline">
           <div class="primary_panel" style="margin-top: 20px">
-            //项目名称栏
+            <!-- 项目名称栏 -->
             <div class="row">
-              <label for="name">项目名称</label> <el-input id="name" v-model="name" placeholder="请输入名称" clearable></el-input>
+              <label for="ProjectName">项目名称:</label><br>
+              <p>投资项目1</p>
+            </div>
+
+            <!-- 项目信息 -->
+            <div class="row">
+              <label for="UserName">来自用户:</label><br>
+              <p>用户1</p>
             </div>
             
             <div class="row">
-              <label for="money">拆借金额</label><el-input id="money" v-model="money" placeholder="请输入金额" clearable></el-input>
+              <label for="Contact">联系方式:</label><br>
+              <p>电话:10086</p>
+            </div>
+            
+
+
+            <div class="row">
+              <!-- 进度条显示 -->
+              <label for="money">剩余金额/总金额:</label><p>40 / 50</p>
+              <el-progress :text-inside="true" :stroke-width="18" :percentage="80" color="rgba(142, 113, 199, 0.7)"></el-progress>
+            </div>
+            
+
+            <!-- 增加剩余XX天 -->
+            <div class="row">
+              <label>开始日期:</label><br>
+              <p>2018/2/30</p>
             </div>
             
             <div class="row">
-              <label>开始日期</label><br>
-            </div>
-            
-            <div class="row">
-              <label>截止日期</label><br>
+              <label>截止日期:</label><br>
+              <p>2018/2/30</p>
+              <p>剩余</p><label for="restDay">2</label><p>天</p>
             </div>
             
             <div class="row">
               <label>还款日期</label><br>
+              <p>2018/2/30</p>
             </div>
-          </div>
 
-        //短期投资  
-          <div id="consume" style="margin-top: 20px">
+
             <div class="row">
-              <label>资金用途详述</label><br>
+              <label>详细信息</label>
             </div>
+            <template id="Inform">
+              <el-tabs  class="row" v-model="activeName" @tab-click="handleClick">
+                <el-tab-pane label="项目介绍" name="first">项目介绍</el-tab-pane>
+                <el-tab-pane label="相关政策" name="second">相关政策</el-tab-pane>
+              </el-tabs>
+            </template>
+            <br><br><br>
 
-            <div class="row" style="margin-left: 250px">
-              <el-button type="primary" round>提交</el-button>
-            </div>
-
-          </div>
-
-        //学习类长期投资    
-          <div id="learn" style="margin-top: 20px;display: none">
             <div class="row">
-              <label>项目说明</label><br>
+              <label>请选择投资金额</label>
             </div>
+            <el-input v-model="input" placeholder="请输入金额"></el-input>
 
-            <div class="row" style="margin-left: 250px">
-              <el-button type="primary" round>提交</el-button>
-            </div>
-
+            <el-button class="checkbutton" icon="success"> 确认 </el-button>
           </div>
         </div>
 
@@ -68,7 +86,7 @@
     import ElCard from "element-ui/packages/card/src/main";
 
     export default {
-      name: "loan",
+      name: "DoInvesting",
       components:{
         ElCard,
         navi},
@@ -77,62 +95,12 @@
           //返回查看投资项目界面
           this.$router.go(-1);
         },
-        change1(){
-          document.getElementById("learn").style.display = "none";
-          document.getElementById("consume").style.display = "inline";
-
-          document.getElementById("con_tab").style.backgroundColor = "lightskyblue";
-          document.getElementById("con_tab").style.color = "black";
-          document.getElementById("learn_tab").style.color = "white";
-          document.getElementById("learn_tab").style.backgroundColor = "rgba(17, 17, 17, 0.17)";
-        },
-        change2(){
-          document.getElementById("consume").style.display = "none";
-          document.getElementById("learn").style.display = "inline";
-
-          document.getElementById("con_tab").style.backgroundColor = "rgba(17, 17, 17, 0.17)";
-          document.getElementById("con_tab").style.color = "white";
-          document.getElementById("learn_tab").style.color = "black";
-          document.getElementById("learn_tab").style.backgroundColor = "lightskyblue";
+        data() {
+          return {
+            input: ''
+          }
         }
-      },
-
-      data(){
-        return {
-          pickerOptions1: {
-            disabledDate(time) {
-              return time.getTime() > Date.now();
-            },
-          },
-          date1: '',
-          date2: '',
-          date3: '',
-          usage_radio: 3,
-          textarea1: '',
-          tableData: [{
-            name: '融资项目一',
-            num: '1',
-            date: '2018/9/17',
-            action:''
-          }, {
-            name: '融资项目二',
-            num: '2',
-            date: '2018/9/17',
-            action:''
-          }, {
-            name: '融资项目三',
-            num: '3',
-            date: '2018/9/17',
-            action:''
-          }, {
-            name: '融资项目四',
-            num: '4',
-            date: '2018/9/17',
-            action:''
-          }]
-        };
-      },
-
+      }
     }
 </script>
 
@@ -141,15 +109,6 @@
     /*.body{
       background-color: #5f9ea0;
     }*/
-
-    .left_label{
-      width:200px;
-      height:50px;
-      /*margin-top: 20px;*/
-      border: 1px black solid;
-      text-align:center;
-      padding-top: 17px;
-    }
 
     .choose_panel{
       display: flex;
@@ -187,16 +146,19 @@
       display:flex;
     }
     .left{
-      border:1px black solid;
       width:200px;
       height: 150px;
-      margin-top: 100px;
+      margin-top: 300px;
+      margin-left: 50px;
       padding-top: 20px;
       position: fixed;
     }
     .sheet{
+      background: #a0c7f5;
       margin-top: 100px;
-      margin-left:25%;
+      margin-bottom: 100px;
+      width: 600px;
+      margin-left:30%;
     }
     .chooseButton{
       margin-left: 15%;
@@ -204,6 +166,11 @@
     .row{
       width:400px;
       padding: 10px;
+      margin-left: 20px;
+    }
+    .checkbutton{
+      margin-left: 40%;
+
     }
 
 </style>
