0bc5c1eeb69ca9bca9a4d02c3ab90f691a282307
Young <895254752@qq.com>
2018-08-25 21:54:03 +0800

    根据8.21的需求调整了Financial.vue的结构

--- src/pages/FinancialStanding.vue
+++ src/pages/FinancialStanding.vue
@@ -3,128 +3,204 @@
     <div>
       <div class="chooseButton">
         <el-row>
-          <el-button id="income_btn" @click="change_income">收入</el-button>
-          <el-button id="outcome_btn" @click="change_outcome">支出</el-button>
-          <el-button id="othercome_btn" @click="change_othercome">其他</el-button>
+          <el-button id="financial_btn" @click="change_financial">财务</el-button>
+          <el-button id="loan_btn" @click="change_loan">借款</el-button>
         </el-row>
+        <hr/>
       </div>
-      <hr/>
-      <div class="FinDateChoice">
-        <div class="block">
-          <span class="demonstration">请选择你要查看的时间区间段</span>
-          <el-date-picker
-            v-model="date_value_choose"
-            type="daterange"
-            align="right"
-            unlink-panels
-            range-separator="到"
-            start-placeholder="开始日期"
-            end-placeholder="结束日期"
-            :picker-options="pickerOptions2">
-          </el-date-picker>
-        </div>
-      </div>
-      <hr/>
-      <div class="LevelTwoIndex" id="leveltwocheck">
-        <div>
-          <div style="margin-top: 20px">
-            <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选（此处暂时有点bug，全选后不能立刻显示）</el-checkbox>
-            <el-checkbox-group v-model="checkboxGroup2" size="medium" @change="handleCheckedCitiesChange">
-              <el-checkbox-button v-for="indexA in indexAs" :label="indexA" :key="indexA" >{{indexA}}</el-checkbox-button>
-            </el-checkbox-group>
+      <div id="financial_id">
+        <div class="FinDateChoice">
+          <div class="block">
+            <span class="demonstration">请选择你要查看的时间区间段</span>
+            <el-date-picker
+              v-model="date_value_choose"
+              type="daterange"
+              align="right"
+              unlink-panels
+              range-separator="到"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+              :picker-options="pickerOptions2">
+            </el-date-picker>
           </div>
         </div>
-      </div>
-      <hr/>
-      <div style="text-align: justify">
-        <el-button type="primary" round>&nbsp;提交&nbsp;</el-button>
-      </div>
-      <hr/>
-      <div class="LevelOneIndex" id="leveloneindex">
+        <hr/>
+        <div class="chooseButton">
+          <el-row>
+            <el-button id="income_btn" @click="change_income">收入</el-button>
+            <el-button id="outcome_btn" @click="change_outcome">支出</el-button>
+            <el-button id="othercome_btn" @click="change_othercome">其他</el-button>
+          </el-row>
+        </div>
+        <hr/>
+        <div class="LevelTwoIndex" id="leveltwocheck">
+          <div>
+            <div style="margin-top: 20px">
+              <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选（此处暂时有点bug，全选后不能立刻显示）</el-checkbox>
+              <el-checkbox-group v-model="checkboxGroup2" size="medium" @change="handleCheckedCitiesChange">
+                <el-checkbox-button v-for="indexA in indexAs" :label="indexA" :key="indexA" >{{indexA}}</el-checkbox-button>
+              </el-checkbox-group>
+            </div>
+          </div>
+        </div>
+        <hr/>
+        <div style="text-align: justify">
+          <el-button type="primary" round>&nbsp;提交&nbsp;</el-button>
+        </div>
+        <hr/>
+        <div class="LevelOneIndex" id="leveloneindex">
 
-        <div class="class_income" id="id_class_income">
-          <p style="font-size: 1em;">此处展示{{date_value_choose.toString()}}的收入数据</p>
-          <div id="myIncomeOverview">
-            <h4><b>您的收入为总额为：<i class="el-icon-menu" style="color: #409EFF"></i>&nbsp 6050 元</b></h4>
+          <div class="class_income" id="id_class_income">
+            <p style="font-size: 1em;">此处展示{{date_value_choose.toString()}}的收入数据</p>
+            <div id="myIncomeOverview">
+              <h4><b>您的收入为总额为：<i class="el-icon-menu" style="color: #409EFF"></i>&nbsp 6050 元</b></h4>
+            </div>
+            <hr/>
+            <h4><b>您本月的收入变化</b></h4>
+            <div id="myIncomeBar" :style="{width: '800px', height: '300px'}"></div>
           </div>
-          <hr/>
-          <h4><b>您本月的收入变化</b></h4>
-          <div id="myIncomeBar" :style="{width: '800px', height: '300px'}"></div>
-        </div>
 
-        <div class="class_outcome" id="id_class_outcome" style="display: none">
-          <p style="font-size: 1em">此处展示{{date_value_choose.toString()}}的支出数据</p>
-          <h4><b>您本月的支出情况</b></h4>
-          <div class="table-responsive" style="text-indent: 5px;max-width: 700px">
-            <table class="table table-bordered">
-              <!--<caption><b>您的信用评级为：100</b></caption>-->
-              <thead>
-              <tr>
-                <th><i class="el-icon-info" style="color: #409EFF"></i>&nbsp支出总额</th>
-                <th><i class="el-icon-tickets" style="color: #409EFF"></i>&nbsp刚性支出</th>
-                <th><i class="el-icon-sold-out" style="color: #409EFF"></i>&nbsp可调指出的总额</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr>
-                <td>70</td>
-                <td>30</td>
-                <td>100</td>
-              </tr>
-              </tbody>
-            </table>
+          <div class="class_outcome" id="id_class_outcome" style="display: none">
+            <p style="font-size: 1em">此处展示{{date_value_choose.toString()}}的支出数据</p>
+            <h4><b>您本月的支出情况</b></h4>
+            <div class="table-responsive" style="text-indent: 5px;max-width: 700px">
+              <table class="table table-bordered">
+                <!--<caption><b>您的信用评级为：100</b></caption>-->
+                <thead>
+                <tr>
+                  <th><i class="el-icon-info" style="color: #409EFF"></i>&nbsp支出总额</th>
+                  <th><i class="el-icon-tickets" style="color: #409EFF"></i>&nbsp刚性支出</th>
+                  <th><i class="el-icon-sold-out" style="color: #409EFF"></i>&nbsp可调指出的总额</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>70</td>
+                  <td>30</td>
+                  <td>100</td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+            <hr/>
+            <h4><b>您本月的支出变化</b></h4>
+            <div id="myOutcomeBar" :style="{width: '800px', height: '300px'}"></div>
+            <hr/>
+            <h4><b>您本月的支出分布统计图</b></h4>
+            <div id="myOutcomePie" :style="{width: '1000px', height: '450px'}"></div>
           </div>
-          <hr/>
-          <h4><b>您本月的支出变化</b></h4>
-          <div id="myOutcomeBar" :style="{width: '800px', height: '300px'}"></div>
-          <hr/>
-          <h4><b>您本月的支出分布统计图</b></h4>
-          <div id="myOutcomePie" :style="{width: '1000px', height: '450px'}"></div>
-        </div>
 
-        <div class="class_othercome"id="id_class_othercome" style="display: none">
-          <p style="font-size: 1em">此处展示{{date_value_choose.toString()}}的其他数据</p>
-        </div>
+          <div id="mySurplus">
+            <hr>
+            <h4><b>您的结余为：<i class="el-icon-success" style="color: #409EFF"></i>&nbsp 3752.30 元</b></h4>
+            <hr/>
+          </div>
 
+          <div class="class_othercome"id="id_class_othercome" style="display: none">
+            <p style="font-size: 1em">此处展示{{date_value_choose.toString()}}的其他数据</p>
+            <h4><b>您的负债总额为：<i class="el-icon-success" style="color: #409EFF"></i>&nbsp 6632.30 元</b></h4>
+            <hr/>
+            <h4><b>您本月的蚂蚁花呗情况</b></h4>
+            <div id="myAntBar" style="width: 1000px; height: 300px; "></div>
+            <hr/>
+          </div>
+
+        </div>
+        <div class="LevelTwoIndex" id="leveltwoindex">
+          <p style="font-size: 1em;">此处展示{{checkboxGroup2.toString()}}的二级指标数据</p>
+          <p id="checktest" style="font-size: 1em; display: none">此处展示净资产数据</p>
+          <div class="LevelTwoPanel">
+            <div id="Index_NetWorth">
+              <h4><i class="el-icon-info"></i><b>&nbsp&nbsp您的净资产为：&nbsp 6050 元</b></h4>
+              <hr/>
+            </div>
+            <div id="Index_EngelsCoefficient">
+              <h4><i class="el-icon-success"></i><b>&nbsp&nbsp您的恩格尔系数为：&nbsp 55%,&nbsp&nbsp在同学中处于中等水平</b></h4>
+              <hr/>
+            </div>
+            <div id="Index_RigidRatio">
+              <h4><i class="el-icon-goods"></i><b>&nbsp&nbsp您的刚性比率是:&nbsp 60%，当前可支配收入较多，建议后期缩减开支</b></h4>
+              <hr/>
+            </div>
+            <div id="Index_AssetLiabilityRatio">
+              <h4><i class="el-icon-document"></i><b>&nbsp&nbsp您的资产负债率为：&nbsp 25%</b></h4>
+              <hr/>
+            </div>
+            <div id="Index_Solvency">
+              <h4><i class="el-icon-tickets"></i><b>&nbsp&nbsp您的偿债能力为：&nbsp 100%</b></h4>
+              <hr/>
+            </div>
+            <div id="Index_Leverage">
+              <h4><i class="el-icon-news"></i><b>&nbsp&nbsp您的杠杆比例为：&nbsp 35%</b></h4>
+              <hr/>
+            </div>
+            <div id="Index_MonthConsumptionRatio">
+              <h4><i class="el-icon-date"></i><b>&nbsp&nbsp您的月消费比率为：&nbsp 31.4%，在同学中处于中等水平，您倾向于预期消费</b></h4>
+              <hr/>
+            </div>
+            <div id="Index_MonthlySavingsRatio">
+              <h4><i class="el-icon-date"></i><b>&nbsp&nbsp您的月储蓄比例为：&nbsp 23.9%</b></h4>
+              <hr/>
+            </div>
+          </div>
+        </div>
       </div>
-      <hr/>
-      <div class="LevelTwoIndex" id="leveltwoindex">
-        <p style="font-size: 1em;">此处展示{{checkboxGroup2.toString()}}的二级指标数据</p>
-        <p id="checktest" style="font-size: 1em; display: none">此处展示净资产数据</p>
-        <div class="LevelTwoPanel">
-          <div id="Index_NetWorth">
-            <h4><i class="el-icon-info"></i><b>&nbsp&nbsp您的净资产为：&nbsp 6050 元</b></h4>
-            <hr/>
+      <div id="loan_id" style="display: none">
+        <h3><b>消费修正建议</b></h3>
+        <hr/>
+        <h4>1、一性还本付息、先息后本</h4>
+        <div class="LoanPanel">
+          <p>距离下次还款还有<b class="LoanHighLight"> 36 </b>天</p>
+          <p>在您借款成功后第n个月内，根据历史消费记录预测，可知结余占用率、可调支出占用率、需要的额外收入金额如下：</p>
+          <div style="width: 640px" id="loanTable">
+            <template>
+              <el-table
+                :data="tableData"
+                height="250"
+                border
+                style="width: 100%">
+                <el-table-column
+                  prop="month"
+                  label="月份"
+                  width="80">
+                </el-table-column>
+                <el-table-column
+                  prop="valX"
+                  label="结余占用率X"
+                  width="180">
+                </el-table-column>
+                <el-table-column
+                  prop="valY"
+                  label="可调支出占用率Y"
+                  width="180">
+                </el-table-column>
+                <el-table-column
+                  prop="valZ"
+                  label="额外收入金额Z"
+                  width="180">
+                </el-table-column>
+              </el-table>
+            </template>
+            <br/>
           </div>
-          <div id="Index_EngelsCoefficient">
-            <h4><i class="el-icon-success"></i><b>&nbsp&nbsp您的恩格尔系数为：&nbsp 55%,&nbsp&nbsp在同学中处于中等水平</b></h4>
-            <hr/>
-          </div>
-          <div id="Index_RigidRatio">
-            <h4><i class="el-icon-goods"></i><b>&nbsp&nbsp您的刚性比率是:&nbsp 60%，当前可支配收入较多，建议后期缩减开支</b></h4>
-            <hr/>
-          </div>
-          <div id="Index_AssetLiabilityRatio">
-            <h4><i class="el-icon-document"></i><b>&nbsp&nbsp您的资产负债率为：&nbsp 25%</b></h4>
-            <hr/>
-          </div>
-          <div id="Index_Solvency">
-            <h4><i class="el-icon-tickets"></i><b>&nbsp&nbsp您的偿债能力为：&nbsp 100%</b></h4>
-            <hr/>
-          </div>
-          <div id="Index_Leverage">
-            <h4><i class="el-icon-news"></i><b>&nbsp&nbsp您的杠杆比例为：&nbsp 35%</b></h4>
-            <hr/>
-          </div>
-          <div id="Index_MonthConsumptionRatio">
-            <h4><i class="el-icon-date"></i><b>&nbsp&nbsp您的月消费比率为：&nbsp 31.4%，在同学中处于中等水平，您倾向于预期消费</b></h4>
-            <hr/>
-          </div>
-          <div id="Index_MonthlySavingsRatio">
-            <h4><i class="el-icon-date"></i><b>&nbsp&nbsp您的月储蓄比例为：&nbsp 23.9%</b></h4>
-            <hr/>
-          </div>
+          <p>因此，在第1、3、7...个月内，建议您酌情调整下图中占比较大的前几项；在第2、3、7...个月内，建议您酌情考虑兼职、奖学金、相关理财收入等额外收入。</p>
         </div>
+        <hr/>
+        <h4>2、等额本金、等额本息</h4>
+        <div class="LoanPanel">
+          <p>距离最终还款还有<b class="LoanHighLight"> 60 </b>天</p>
+          <p v-if=" valueX<1 ">
+            在您的还款期内，根据历史消费记录预测，可知扣去现有负债后结余占用率为：{{ valueX }}
+          </p>
+          <p v-if=" (valueY>0)&&(valueZ==0)">
+            在您的还款期内，根据历史消费记录预测，可知扣去现有负债和预测结余后还需还款M-K(N)+Q元。如无兼职、奖学金、相关理财收入等额外收入，可调支出占用率为：Y，建议您酌情调整下图中占比较大的前几项。
+          </p>
+          <p>
+            在您的还款期内，根据历史消费记录预测，可知扣去负债、预测结余和所有可调支出后，还需还款M-K(N)+Q元，建议您酌情考虑兼职、奖学金、相关理财收入等额外收入。
+          </p>
+          
+        </div>
+
       </div>
     </div>
   </personalCenter>
@@ -132,6 +208,7 @@
 
 <script>
   import personalCenter from "../components/personalCenter";
+  import InvestList from "../components/investList";
   // 引入基本模板
   let echarts = require('echarts/lib/echarts')
   // 引入柱状图组件
@@ -147,7 +224,7 @@
 
   export default {
   name:"FinancialStanding",
-    components: {personalCenter},
+    components: {InvestList, personalCenter},
     data() {
       return {
         pickerOptions2: {
@@ -177,7 +254,71 @@
             }
           }]
         },
-        // value6: '',
+        tableData: [{
+          month: '1',
+          valX: '15%',
+          valY: '16%',
+          valZ: '27%'
+        }, {
+          month: '2',
+          valX: '15%',
+          valY: '16%',
+          valZ: '27%'
+        }, {
+          month: '3',
+          valX: '15%',
+          valY: '16%',
+          valZ: '27%'
+        }, {
+          month: '4',
+          valX: '15%',
+          valY: '16%',
+          valZ: '27%'
+        }, {
+          month: '5',
+          valX: '15%',
+          valY: '16%',
+          valZ: '27%'
+        }, {
+          month: '6',
+          valX: '15%',
+          valY: '16%',
+          valZ: '27%'
+        }, {
+          month: '7',
+          valX: '15%',
+          valY: '16%',
+          valZ: '27%'
+        }, {
+          month: '8',
+          valX: '15%',
+          valY: '16%',
+          valZ: '27%'
+        }, {
+          month: '9',
+          valX: '15%',
+          valY: '16%',
+          valZ: '27%'
+        }, {
+          month: '10',
+          valX: '17%',
+          valY: '16%',
+          valZ: '27%'
+        }, {
+          month: '11',
+          valX: '15%',
+          valY: '16%',
+          valZ: '27%'
+        }, {
+          month: '12',
+          valX: '15%',
+          valY: '16%',
+          valZ: '27%'
+          }
+        ],
+        valueX: '0.5',
+        valueY: '0.7',
+        valueZ: '0',
         date_value_choose: '',
         checkboxGroup2: [],
         indexAs: indexAOptions,
@@ -189,6 +330,7 @@
       this.drawIncomeBar();
       this.drawOutcomeBar();
       this.drawOutcomePie();
+      this.drawAntBar();
       this.showIndexs();
     },
     methods: {
@@ -202,19 +344,32 @@
         this.isIndeterminate = checkedCount > 0 && checkedCount < this.indexAs.length;
         this.showIndexs();
       },
+      change_financial(){
+        document.getElementById("financial_id").style.display = "inline";
+        document.getElementById("loan_id").style.display = "none";
+      },
+      change_loan(){
+        document.getElementById("financial_id").style.display = "none";
+        document.getElementById("loan_id").style.display = "inline";
+
+      },
+
       change_income(){
         document.getElementById("id_class_income").style.display = "inline";
+        document.getElementById("mySurplus").style.display = "inline";
         document.getElementById("id_class_outcome").style.display = "none";
         document.getElementById("id_class_othercome").style.display = "none";
       },
       change_outcome(){
         document.getElementById("id_class_income").style.display = "none";
         document.getElementById("id_class_outcome").style.display = "inline";
+        document.getElementById("mySurplus").style.display = "inline";
         document.getElementById("id_class_othercome").style.display = "none";
       },
       change_othercome(){
         document.getElementById("id_class_income").style.display = "none";
         document.getElementById("id_class_outcome").style.display = "none";
+        document.getElementById("mySurplus").style.display = "none";
         document.getElementById("id_class_othercome").style.display = "inline";
       },
       showIndexs(){
@@ -449,6 +604,67 @@
         });
       },
 
+      drawAntBar() {
+        // 基于准备好的dom，初始化echarts实例
+        let myAntBar = echarts.init(document.getElementById('myAntBar'))
+        // 绘制图表
+        myAntBar.setOption({
+          tooltip : {
+            trigger: 'axis',
+            axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+              type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+          },
+          legend: {
+            data:['花呗支出', '花呗还款']
+          },
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+          yAxis : [
+            {
+              type : 'value'
+            }
+          ],
+          xAxis : [
+            {
+              type : 'category',
+              axisTick : {show: false},
+              data : ["1号", "2号", "3号", "4号", "5号", "6号","7号","8号", "9号", "10号", "11号", "12号", "13号","14号","15号", "16号", "17号", "18号", "19号", "20号","21号","22号", "23号", "24号", "25号", "26号", "27号","28号"]
+            }
+          ],
+          series : [
+            {
+              name:'花呗支出',
+              type:'bar',
+              stack: '总量',
+              label: {
+                normal: {
+                  show: true
+                }
+              },
+              data:[20, 36, 10, 40, 20, 60, 5, 20, 36, 10, 40, 20, 60, 5, 20, 36, 10, 40, 20, 60,5, 20, 36, 10, 40, 20, 60, 5]
+            },
+            {
+              name:'花呗还款',
+              type:'bar',
+              stack: '总量',
+              label: {
+                normal: {
+                  show: true,
+                  position: 'left'
+                }
+              },
+              data:[-5, -20, -36, -10, -40, -20, -80, -5, -20, -36, -10, -40, -20, -60, -5, -20, -36, -10, -40, -20, -60, -5, -20, -36, -10, -40, -20, -60]
+            }
+          ]
+        });
+      },
+
+
 
     }
   }
@@ -460,5 +676,15 @@
   .LevelTwoPanel i{
     color: #409EFF;
   }
+
+  .LoanPanel {
+    margin-left: 20px;
+    margin-right: 20px;
+  }
+
+  .LoanHighLight {
+    font-size: 22px;
+  }
+
 
 </style>
