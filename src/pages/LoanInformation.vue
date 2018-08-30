c00c71539b21e0998694ce23d95df4ebdc36d568
Young <895254752@qq.com>
2018-08-30 10:49:39 +0800

    网页结构调整

---
+++ src/pages/LoanInformation.vue
@@ -0,0 +1,80 @@
+<template>
+  <personalCenter paneltitle="项目信息">
+    <div class="projectPanel">
+      <div class="chooseButton">
+        <el-row style="text-align: center">
+          <el-button id="invest_recent_btn" class="type_choose_btn" @click="change_invest_recently" autofocus="true" round>财务</el-button>
+          <el-button id="loan_recent_btn"  class="type_choose_btn" @click="change_loan_recently" round>借款</el-button>
+        </el-row>
+        <hr/>
+      </div>
+      <div class="projectPages">
+        <project-list></project-list><br/>
+        <project-list></project-list><br/>
+        <project-list></project-list><br/>
+        <project-list></project-list><br/>
+      </div>
+      <div id="poj_pagination">
+        <div class="block">
+          <!--<span class="demonstration">完整功能</span>-->
+          <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page="currentPage1"
+            :page-sizes="[10, 20, 30, 40]"
+            :page-size="10"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="40">
+          </el-pagination>
+        </div>
+      </div>
+
+    </div>
+  </personalCenter>
+</template>
+
+<script>
+  import personalCenter from "../components/personalCenter";
+  import ProjectList from "../components/projectList";
+
+  export default {
+    name:"loaninformation",
+    components: {ProjectList, personalCenter},
+    methods: {
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
+      }
+    },
+    data() {
+      return {
+        currentPage1: 2,
+      };
+    },
+    methods:{
+      change_invest_recently(){
+
+      },
+      change_loan_recently(){
+
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  #poj_pagination{
+    text-align: center;
+    margin-top: 30px;
+    margin-bottom: 20px;
+  }
+
+  .type_choose_btn{
+    padding-left: 50px;
+    padding-right: 50px;
+  }
+
+</style>
--- src/pages/UserSpace.vue
+++ src/pages/UserSpace.vue
@@ -1,6 +1,22 @@
 <template>
   <personalCenter paneltitle="账户总览">
-
+    <el-row :gutter="12">
+      <el-col :span="8">
+        <el-card shadow="always" id="card_test">
+          总是显示
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card shadow="hover">
+          鼠标悬浮时显示
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card shadow="never">
+          从不显示
+        </el-card>
+      </el-col>
+    </el-row>
   </personalCenter>
 </template>
 
@@ -19,4 +35,13 @@
   }
 
 </script>
+
+<style scoped>
+  #card_test {
+    background-image: url("../../static/pic/photo.jpg");
+    width: 249px;
+    height: 253px;
+  }
+
+</style>
 
