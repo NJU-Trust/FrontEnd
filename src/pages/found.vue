c8962267e53fa3ca69d8a0df913df65890f351ef
NiuHaoYue98 <1113080946@qq.com>
2018-09-09 16:21:53 +0800

    test

--- src/pages/found.vue
+++ src/pages/found.vue
@@ -19,21 +19,109 @@
           <el-tabs type="border-card">
             <el-tab-pane >
               <span slot="label" style="font-size:19px;"><i class="el-icon-message"></i>&nbsp;&nbsp;最新消息</span>
-              <div style="position:relative;top:20px;margin-left:5%;margin-right:5%;">
+              <div style="position:relative;top:10px;margin-left:5%;margin-right:5%;">
                 <template>
                   <el-carousel :interval="3000" type="card" height="200px">
-                    <el-carousel-item v-for="i in 4" :key="i">
+                    <el-carousel-item v-for="i in latestmes.length" :key="i">
                       <div v-if="i === 1">
-                        <img src="../../static/pic/testkey.png" style="width:360px;height:180px;margin-top:9px;margin-left:13px;" class="picbox" alt="User_pic">
+                        <el-popover
+                          placement="left"
+                          style="background-color: #DCDFE6;"
+                          width="200"
+                          trigger="hover">
+                          <div >
+                                <div style="position:relative;top:3px;">
+                                  <span style="position:relative;left:20px;"><i class="el-icon-time"></i>&nbsp;&nbsp;{{ latestmes[i-1].time}}</span>
+                                  <br/>
+                                  <span style="position:relative;left:20px;"><i class="el-icon-phone"></i>&nbsp;&nbsp;{{ latestmes[i-1].phone}}</span>
+                                  <br/>
+                                  <span style="position:relative;left:20px;"><i class="el-icon-location"></i>&nbsp;&nbsp;{{ latestmes[i-1].loc}}</span>
+                                </div>
+                            <hr/>
+                                <div style="margin-top:-10%;width:150px;">
+                                  <img src="../../static/pic/testuserpic1.png" style="width:30px;height:30px;position:relative;left:10px;top:-15px;"  alt="User_pic" >
+                                  <span style="position:relative;margin-top:-10%;">&nbsp;&nbsp;&nbsp;&nbsp;“{{ latestmes[i-1].dec}}”</span>
+                                </div>
+                              </div>
+                          <el-button slot="reference">
+                            <img src="../../static/pic/testkey.png" style="width:360px;height:180px;margin-top:-1%;margin-left:-3%;" class="picbox" alt="User_pic" >
+                          </el-button>
+                        </el-popover>
                       </div>
                       <div v-if="i === 2">
-                        <img src="../../static/pic/testusb.png" style="width:360px;height:180px;margin-top:9px;margin-left:13px;" class="picbox" alt="User_pic">
+                        <el-popover
+                          placement="left"
+                          style="background-color: #DCDFE6;"
+                          width="200"
+                          trigger="hover">
+                          <div class="textitem">
+                            <div style="position:relative;top:3px;">
+                              <span style="position:relative;left:20px;"><i class="el-icon-time"></i>&nbsp;&nbsp;{{ latestmes[i-1].time}}</span>
+                              <br/>
+                              <span style="position:relative;left:20px;"><i class="el-icon-phone"></i>&nbsp;&nbsp;{{ latestmes[i-1].phone}}</span>
+                              <br/>
+                              <span style="position:relative;left:20px;"><i class="el-icon-location"></i>&nbsp;&nbsp;{{ latestmes[i-1].loc}}</span>
+                            </div>
+                            <hr/>
+                            <div style="margin-top:-10%;width:150px;">
+                              <img src="../../static/pic/testuserpic2.png" style="width:30px;height:30px;position:relative;left:10px;top:-15px;"  alt="User_pic" >
+                              <span style="position:relative;margin-top:-10%;">&nbsp;&nbsp;&nbsp;&nbsp;“{{ latestmes[i-1].dec}}”</span>
+                            </div>
+                          </div>
+                          <el-button slot="reference">
+                            <img src="../../static/pic/testusb.png" style="width:360px;height:180px;margin-top:-1%;margin-left:-3%;" class="picbox" alt="User_pic" >
+                          </el-button>
+                        </el-popover>
                       </div>
                       <div v-if="i === 3">
-                        <img src="../../static/pic/testcard.png" style="width:360px;height:180px;margin-top:9px;margin-left:13px;" class="picbox" alt="User_pic">
+                        <el-popover
+                          placement="left"
+                          style="background-color: #DCDFE6;"
+                          width="200"
+                          trigger="hover">
+                          <div class="textitem">
+                            <div style="position:relative;top:3px;">
+                              <span style="position:relative;left:20px;"><i class="el-icon-time"></i>&nbsp;&nbsp;{{ latestmes[i-1].time}}</span>
+                              <br/>
+                              <span style="position:relative;left:20px;"><i class="el-icon-phone"></i>&nbsp;&nbsp;{{ latestmes[i-1].phone}}</span>
+                              <br/>
+                              <span style="position:relative;left:20px;"><i class="el-icon-location"></i>&nbsp;&nbsp;{{ latestmes[i-1].loc}}</span>
+                            </div>
+                            <hr/>
+                            <div style="margin-top:-10%;width:150px;">
+                              <img src="../../static/pic/testuserpic3.png" style="width:30px;height:30px;position:relative;left:10px;top:-15px;"  alt="User_pic" >
+                              <span style="position:relative;margin-top:-10%;">&nbsp;&nbsp;&nbsp;&nbsp;“{{ latestmes[i-1].dec}}”</span>
+                            </div>
+                          </div>
+                          <el-button slot="reference">
+                            <img src="../../static/pic/testcard.png" style="width:360px;height:180px;margin-top:-1%;margin-left:-3%;" class="picbox" alt="User_pic" >
+                          </el-button>
+                        </el-popover>
                       </div>
                       <div v-if="i === 4">
-                        <img src="../../static/pic/testcup.png" style="width:360px;height:180px;margin-top:9px;margin-left:13px;" class="picbox" alt="User_pic">
+                        <el-popover
+                          placement="left"
+                          style="background-color: #DCDFE6;"
+                          width="200"
+                          trigger="hover">
+                          <div class="textitem">
+                            <div style="position:relative;top:3px;">
+                              <span style="position:relative;left:20px;"><i class="el-icon-time"></i>&nbsp;&nbsp;{{ latestmes[i-1].time}}</span>
+                              <br/>
+                              <span style="position:relative;left:20px;"><i class="el-icon-phone"></i>&nbsp;&nbsp;{{ latestmes[i-1].phone}}</span>
+                              <br/>
+                              <span style="position:relative;left:20px;"><i class="el-icon-location"></i>&nbsp;&nbsp;{{ latestmes[i-1].loc}}</span>
+                            </div>
+                            <hr/>
+                            <div style="margin-top:-10%;width:150px;">
+                              <img src="../../static/pic/photo.jpg" style="width:30px;height:30px;position:relative;left:10px;top:-15px;"  alt="User_pic" >
+                              <span style="position:relative;margin-top:-10%;">&nbsp;&nbsp;&nbsp;&nbsp;“{{ latestmes[i-1].dec}}”</span>
+                            </div>
+                          </div>
+                          <el-button slot="reference">
+                            <img src="../../static/pic/testcup.png" style="width:360px;height:180px;margin-top:-1%;margin-left:-3%;" class="picbox" alt="User_pic" >
+                          </el-button>
+                        </el-popover>
                       </div>
                       <!--<div class="textitem">
                         <el-tag type="info"
@@ -87,18 +175,57 @@
                   <hr/>
                 </div>
               </template>
+              <div v-for="j in mesdata.length" :key="j">
+              <el-row :gutter="400" >
+                <div v-for="i in 2" :key="i">
+                  <el-col :span="6">
+                    <el-card class="box-card">
+                        <div class="grid-content bg-purple">
+                          <div style="margin-top:-3%;">
+                          <img src="../../static/pic/testuserpic1.png" style="width:30px;height:30px;position:relative;left:10px;"  alt="User_pic" >
+                          <span style="position:relative;left:20px;">{{ mesdata[j-1][i-1].username}}</span>
+                          <span style="position:relative;left:135px;">{{ mesdata[j-1][i-1].time}}</span>
+                          </div>
+                          <br/>
+                          <el-row :gutter="30">
+                            <el-col :span="13">
+                              <div class="grid-content bg-purple" style="margin-top:-3%;">
+                                <span style="position:relative;left:55px;"><i class="el-icon-phone"></i>&nbsp;&nbsp;{{ mesdata[j-1][i-1].phone}}</span>
+                                <br/>
+                                <span style="position:relative;left:55px;"><i class="el-icon-location"></i>&nbsp;&nbsp;{{ mesdata[j-1][i-1].loc}}</span>
+                                <br/>
 
-
+                                <div style="position:relative;top:3px;width:200px;">
+                                  <span style="position:relative;left:20px;">&nbsp;&nbsp;&nbsp;&nbsp;{{ mesdata[j-1][i-1].dec}}</span>
+                                </div>
+                              </div>
+                            </el-col>
+                            <el-col :span="10">
+                              <div class="grid-content bg-purple" style="margin-top:-9%;">
+                                <img v-bind:src=mesdata[j-1][i-1].pic style="width:110px;height:110px;position:relative;left:5px;top:-8px;" class="picbox" alt="User_pic">
+                                <br/>
+                                <span style="position:relative;"><strong>{{ mesdata[j-1][i-1].mestype}}&nbsp;&nbsp;>></strong></span>
+                                <span style="position:relative;left:5px;"><strong>{{ mesdata[j-1][i-1].name}}</strong></span>
+                              </div>
+                            </el-col>
+                          </el-row>
+                      </div>
+                    </el-card>
+                    </el-col>
+                  </div>
+              </el-row>
+              </div>
               <el-pagination
-                background
-                layout="prev, pager, next"
-                style="position:absolute;left:450px;top:630px;"
-                :total="100">
-              </el-pagination>
+                  background
+                  layout="prev, pager, next"
+                  style="position:relative;left:350px;"
+                  :total="1000">
+                </el-pagination>
             </el-tab-pane>
           </el-tabs>
-          <hr/>
         </div>
+        <br/>
+        <hr/>
 
         <!--<div style="position:relative;top:20px;margin-left:5%;margin-right:5%;">
           <template>
@@ -203,8 +330,6 @@
   import footerBar from '@/components/footerBar.vue';
   import rightBar from '@/components/rightBar.vue';
   import leftInformationbar from "@/components/leftInformationbar.vue"
-
-  //const types = ['校园卡', '钥匙', '水杯', '雨伞','其他'];
   export default {
     name: "found",
     components:{leftInformationbar, navi, footerBar, rightBar},
@@ -217,60 +342,96 @@
         list: [],
         loading: false,
         mesdata:[
-          {
+          [{
             mestype:'失物招领',
-            itemtype:'其他',
-            name:'杯子',
+            itemtype:'钥匙',
+            name:'钥匙',
             phone:'13834712391',
-            dec:'下午在仙I-221上课的时候捡到的一个杯子，放到仙I的保卫处了。（P.s.竟然和我的杯子是同款）',
+            dec:'跑步机旁边捡到的，交给体育馆门口的叔叔了',
+            pic:"../../static/pic/testkey.png",
+            time:'2018-08-24 18:50:35',
+            state:true,
+            username:'刘一一',
+            loc:'方肇周体育馆',
+          },{
+            mestype:'寻物启事',
+            itemtype:'雨伞',
+            name:'雨伞',
+            phone:'15589210472',
+            dec:'落在四食门口的一辆小蓝里了！给了四食超市阿姨。',
             pic:'../../static/pic/mestest.png',
-            time:'2018-08-24',
+            time:'2018-09-06 09:45:14',
             state:true,
             username:'徐忘',
+            loc:'共享单车',
+          }],[{
+            mestype:'寻物启事',
+            itemtype:'校园卡',
+            name:'校园卡',
+            phone:'15837196710',
+            dec:'就在教超阿姨那里',
+            pic:'../../static/pic/mestest.png',
+            time:'2018-09-05 21:59:24',
+            state:true,
+            username:'孟哲宁',
+            loc:'教超',
           },{
             mestype:'失物招领',
             itemtype:'其他',
+            name:'U盘',
+            phone:'18842957391',
+            dec:'明天机房有考试呀，今天落在机房的U盘都收到119了哦',
+            pic:'../../static/pic/mestest.png',
+            time:'2018-09-07 12:34:40',
+            state:true,
+            username:'唐月',
+            loc:'基础实验楼乙124',
+          }],[{
+            mestype:'失物招领',
+            itemtype:'水杯',
             name:'杯子',
-            phone:'13834712391',
+            phone:'13878916391',
             dec:'下午在仙I-221上课的时候捡到的一个杯子，放到仙I的保卫处了。（P.s.竟然和我的杯子是同款）',
             pic:'../../static/pic/mestest.png',
-            time:'2018-08-24',
+            time:'2018-09-04 00:12:39',
             state:true,
-            username:'徐忘',
+            username:'王佳昕',
+            loc:'仙I-221',
           },{
             mestype:'失物招领',
-            itemtype:'其他',
-            name:'杯子',
-            phone:'13834712391',
-            dec:'下午在仙I-221上课的时候捡到的一个杯子，放到仙I的保卫处了。（P.s.竟然和我的杯子是同款）',
+            itemtype:'校园卡',
+            name:'校园卡',
+            phone:'18852197520',
+            dec:'落在食堂的餐盘里面了！食堂阿姨说差点扔掉，在哪里放的盘子去哪里拿吧！',
             pic:'../../static/pic/mestest.png',
-            time:'2018-08-24',
+            time:'2018-09-06 12:50:21',
             state:true,
-            username:'徐忘',
-          },{
-            mestype:'失物招领',
-            itemtype:'其他',
-            name:'杯子',
-            phone:'13834712391',
-            dec:'下午在仙I-221上课的时候捡到的一个杯子，放到仙I的保卫处了。（P.s.竟然和我的杯子是同款）',
-            pic:'../../static/pic/mestest.png',
-            time:'2018-08-24',
-            state:true,
-            username:'徐忘',
-          },{
-            mestype:'失物招领',
-            itemtype:'其他',
-            name:'杯子',
-            phone:'13834712391',
-            dec:'下午在仙I-221上课的时候捡到的一个杯子，放到仙I的保卫处了。（P.s.竟然和我的杯子是同款）',
-            pic:'../../static/pic/mestest.png',
-            time:'2018-08-24',
-            state:true,
-            username:'徐忘',
-          },
-        ],
+            username:'成帆洁',
+            loc:'四五六食堂',
+          }]],
         activeName: '1',
-
+        latestmes:[
+          {
+          phone:'13834712391',
+          dec:'跑步机旁边捡到的，交给体育馆门口的叔叔了',
+          loc:'方肇周体育馆',
+          time:'2018-08-24'
+          },{
+          phone:'18842957391',
+          dec:'明天机房有考试呀，今天落在机房的U盘都收到119了哦',
+          loc:'基础实验楼乙124',
+          time:'2018-09-07'
+          },{
+            phone:'15837196710',
+            dec:'就在教超阿姨那里',
+            loc:'教超',
+            time:'2018-09-05'
+          },{
+            phone:'15589210472',
+            dec:'落在四食门口的一辆小蓝里了！给了四食超市阿姨。',
+            loc:'共享单车',
+            time:'2018-09-06',}
+          ]
       }
     },
     methods: {
@@ -289,7 +450,10 @@
         } else {
           this.options4 = [];
         }
-      }
+      },
+      dialog: function(){
+
+      },
     },
     created:function(){
       this.test();
@@ -303,8 +467,8 @@
     /*background-color: rgba(173,216,230,0.5);*/
     width: 100%;
     background-color: #D9F3FB;
-  //height:100%;
-    min-height:1000px;
+    //height:100%;
+    height:1300px;
   //height: 200px;
     padding-bottom: 20px;
     display:flex;
@@ -337,7 +501,7 @@
     margin-left: 24%;
     background:white;
     border:1px solid #e4e4e4;
-    height:800px;
+    //height:80px;
     box-shadow:
       0 1px 6px 0 rgba(0,0,0, .12),
       0 1px 6px 0 rgba(0,0,0, .12);
@@ -346,8 +510,9 @@
 
   .textitem{
     position:relative;
-    left:200px;
-    top:-170px;
+    //background-color: #d3dce66;
+   // left:200px;
+    //top:-170px;
 
   }
 
@@ -361,21 +526,23 @@
 
   /*卡片样式*/
   .box-card {
-    width: 780px;
-    height: 300px;
-    border-top:3px solid dodgerblue;
+    width: 370px;
+    height: 180px;
+    //border-top:3px solid dodgerblue;
     border-radius: 5px;
     box-shadow:
       0 1px 6px 0 rgba(0,0,0, .12),
       0 1px 6px 0 rgba(0,0,0, .12);
-    margin: 0px;
-    padding: 5px;
-    //position:relative;
-    //left:100px;
+    position:relative;
+    left:40px;
     //top:70px;
   }
   .box-card:hover{
-    background:#D1EEEE;
+    border-color:#4F94CD;
+    border-left:solid #8DB6CD;
+    border-top:solid #8DB6CD;
+
+  //background:#D1EEEE;
   }
 
   .el-dropdown-link {
@@ -414,4 +581,27 @@
     margin-left:5%;
     margin-top:5%;
   }
+  .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    margin-left:-3%;
+    margin-right:-13%;
+    //border-radius: 4px;
+    //min-height: 36px;
+  }
+
 </style>
